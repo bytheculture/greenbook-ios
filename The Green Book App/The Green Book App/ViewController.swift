6a762290d59fe9e4675801aba2d6ff73b686530d
Alyssa <alyssasgable@gmail.com>
2019-06-27 11:46:19 -0500

    Set up xcode project, installed pods, added app icon

---
+++ The Green Book App/The Green Book App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Green Book App
+//
+//  Created by Alyssa Gable on 6/27/19.
+//  Copyright © 2019 By The Culture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The Green Book App/The Green Book AppTests/The_Green_Book_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Green_Book_AppTests.swift
+//  The Green Book AppTests
+//
+//  Created by Alyssa Gable on 6/27/19.
+//  Copyright © 2019 By The Culture. All rights reserved.
+//
+
+import XCTest
+@testable import The_Green_Book_App
+
+class The_Green_Book_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
